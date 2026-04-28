6270d04a42fe67d81cd0c68963d32a1d9cc81545
inlefxto <nicholaidebono@gmail.com>
2026-04-28 20:04:55 +0200

    Update Supabase client & key

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
 export const supabase = createClient(
   'https://ulyjyfvgkshyywykqzus.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVseWp5ZnZna3NoeXl3eWtxenVzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzczMDg3NjQsImV4cCI6MjA5Mjg4NDc2NH0.-LxObK-2v2cZ0J-Qt2FzmlJm441y2_jcFnaSyFhtubk'
+  'sb_publishable_g8ktEjpP84DgF4iThLCoGA_QqxodEV6'
 );
