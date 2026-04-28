901fcffc3868a9748c65cdac82ed210f36eb3f7c
inlefxto <nicholaidebono@gmail.com>
2026-04-28 19:34:08 +0200

    Hardcode supabase.ts values

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,9 +1,5 @@
-console.log('SUPABASE URL:', import.meta.env.VITE_SUPABASE_URL);
-console.log('SUPABASE KEY:', import.meta.env.VITE_SUPABASE_ANON_KEY?.slice(0, 20));
-
 import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL as string;
-const supabaseKey = import.meta.env.VITE_SUPABASE_ANON_KEY as string;
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(
+  'https://ulyjyfvgkshyywykqzus.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVseWp5ZnZna3NoeXl3eWtxenVzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzczMDg3NjQsImV4cCI6MjA5Mjg4NDc2NH0.-LxObK-2v2cZ0J-Qt2FzmlJm441y2_jcFnaSyFhtubk'
+);
